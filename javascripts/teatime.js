052697355989dbc184cebd84988afe8395ce6826
pjleonard37 <pjleonard37@gmail.com>
2015-05-20 21:46:05 -0500

    tea js addition

---
+++ javascripts/teatime.js
@@ -0,0 +1,98 @@
+mixpanel.track("Tea Time Page View");
+ 
+$(function ()
+{
+    herbal.buildTeaBag();
+    black.buildTeaBag();
+    oolong.buildTeaBag();
+    green.buildTeaBag();
+    white.buildTeaBag();
+    mate.buildTeaBag();
+    rooibos.buildTeaBag();
+    chai.buildTeaBag();
+ 
+    $("#teapot").droppable({
+        drop: function (event, ui)
+        {
+            $(this).find("h1").html("Boiling water . . .");
+            brew(ui.draggable);
+        }
+    });
+});
+ 
+function tea(brewTemp, steepTime, steepTimeSeconds, type)
+{
+    this.brewTemp = brewTemp;
+    this.steepTime = steepTime;
+    this.steepTimeSeconds = steepTimeSeconds;
+    this.type = type;
+    this.buildTeaBag = function ()
+    {
+        $("#" + this.type).draggable({ revert: "invalid" });
+    }
+}
+ 
+var herbal = new tea("210", "3-6", 300, "herbal");
+var black = new tea("205", "2-3", 150, "black");
+var oolong = new tea("180", "2-3", 150, "oolong");
+var green = new tea("175", "1-2", 90, "green");
+var white = new tea("155", "1-2", 90, "white");
+var mate = new tea("205", "5-6", 330, "mate");
+var rooibos = new tea("205", "5-6", 330, "rooibos");
+var chai = new tea("212", "10", 600, "chai");
+ 
+function brew($item) {
+    var teaType = window[$item.context.id];
+    mixpanel.track("tea chosen", {
+        "TeaSelection": teaType.type
+    });
+    var teaNotification = $("<h1>You've selected " + teaType.type + " tea. Brew tea at <strong>" + teaType.brewTemp + "\xB0 F</strong>, then steep for <strong>" + teaType.steepTime + " minutes.</strong> Click the pot to begin steeping time. Enjoy!</h1>");
+    $('#instructions').append(teaNotification);
+    $('#teapot').one("click", function ()
+    {
+        clock(teaType.steepTimeSeconds);
+        mixpanel.track("Brewed");
+        $(this).find("h1").html("Steeping tea . . .");
+        $("#teacup").fadeIn(teaType.steepTimeSeconds * 1000);
+    });
+    $item.fadeOut(1600);
+    $(".gallery").fadeOut(2000);
+}
+ 
+function clock(brewTime)
+{
+    var brewTimeStatic = brewTime;
+    var intervalId = setInterval(function ()
+    {
+        var currentSeconds = brewTime % 60;
+        var currentMinutes = (brewTime - currentSeconds) / 60;
+        currentSeconds = (currentSeconds < 10) ? "0" + currentSeconds : currentSeconds;
+        var currentTimeString = currentMinutes + ":" + currentSeconds;
+        if (brewTime < (brewTimeStatic / 4))
+        {
+            $("#teapot").find("img").attr("src", 'Images/TeaPot3.png').fadeIn(30000);
+        }
+        else if (brewTime < (brewTimeStatic / 2))
+        {
+            $("#teapot").find("img").attr("src", 'Images/TeaPot2.png').fadeIn(30000);
+        }
+        else if (brewTime < (brewTimeStatic * 3 / 4))
+        {
+            $("#teapot").find("img").attr("src", 'Images/TeaPot1.png').fadeIn(30000);
+        }
+        $("#teapot").find("h1").html("Steeping time left: " + currentTimeString);
+        if (brewTime == 0)
+        {
+            pour();
+            clearInterval(intervalId);
+        }
+        brewTime--;
+    }, 1000);
+}
+     
+function pour()
+{
+    $("#teapot").find("h1").html("Time to pour . . . ");
+    $("#teapot").find("img").attr("src", 'Images/TeaPot4.png').fadeIn(30000);
+    $("#teacup").find("img").attr("src", 'Images/TeaCupFull.png').fadeIn(30000);
+}
